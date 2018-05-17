e27b8c3656efd47a4627825b6024acc9af59d39e
EmberVoid <jeancarlo199797@gmail.com>
2018-05-17 15:56:31 -0600

    Registering Users

--- src/App.js
+++ src/App.js
@@ -34,9 +34,28 @@ class App extends Component {
             imageURL: '',
             box: {},
             route: 'signin',
-            isSignedIn: false
+            isSignedIn: false,
+            user: {
+                id: '',
+                name: '',
+                email: '',
+                entries: 0,
+                joined: ''
+            }
         }
     }
+
+    loadUser = (data) => {
+        this.setState({
+            user: {
+                id: data.id,
+                name: data.name,
+                email: data.email,
+                entries: data.entries,
+                joined: data.joined
+            }
+        })
+    };
 
     calculateFaceLocation = (data) => {
         const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -89,14 +108,23 @@ class App extends Component {
               {route === 'home'
                   ? <div>
                       <Logo/>
-                      <Rank/>
-                      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+                      <Rank
+                      name ={this.state.user.name}
+                      entries={this.state.user.entries}
+                      />
+                      <ImageLinkForm
+                          onInputChange={this.onInputChange}
+                          onButtonSubmit={this.onButtonSubmit}/>
                       <FaceRecognition box={box} imageURL={imageURL}/>
                   </div>
                   : (
                       route === 'signin'
-                          ? <SignIn onRouteChange={this.onRouteChange}/>
-                          : <Register onRouteChange={this.onRouteChange}/>
+                          ? <SignIn
+                              loadUser={this.loadUser}
+                              onRouteChange={this.onRouteChange}/>
+                          : <Register
+                              loadUser={this.loadUser}
+                              onRouteChange={this.onRouteChange}/>
                   )
               }
           </div>
